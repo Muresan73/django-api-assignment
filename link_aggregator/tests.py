1d8c81ce7e4f12e273d5a780a565b92cd06c7c5a
Grge <grge73@outlook.com>
2022-11-07 20:44:45 +0100

    test datetime

--- link_aggregator/tests.py
+++ link_aggregator/tests.py
@@ -1,19 +1,20 @@
 from django.test import TestCase, Client
 from link_aggregator.models import Link
-
+from datetime import datetime, timedelta, timezone
 # Create your tests here.
 
 
-class APITestCase(TestCase):
+class POSTTestCase(TestCase):
     client = Client()
 
     def test_add_url_to_db(self):
-        response = self.client.post('/api/links', {"url": "betao.se"})
+        response = self.client.post('/api/links', {"url": "https://www.yahoo.com/"})
         self.assertEqual(response.status_code, 201)
-        Link.objects.get(url="betao.se")
+        url = Link.objects.get(url="https://www.yahoo.com/")
+        self.assertTrue(url.created - datetime.now(timezone.utc) < timedelta(minutes=1))
 
     def test_error_on_duplication(self):
-        response = self.client.post('/api/links', {"url": "betao.se"})
+        response = self.client.post('/api/links', {"url": "https://www.yahoo.com/"})
         self.assertEqual(response.status_code, 201)
-        response = self.client.post('/api/links', {"url": "betao.se"})
+        response = self.client.post('/api/links', {"url": "https://www.yahoo.com/"})
         self.assertEqual(response.status_code, 409)
